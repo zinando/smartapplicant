137758f7225c6358d6858d33108ca6095a676434
Samuel Nnadozie <75990760+zinando@users.noreply.github.com>
2025-05-01 13:41:41 +0100

    Move out the backend codes

--- Backend/api/serializers.py
+++
@@ -1,8 +0,0 @@
-# api/serializers.py
-from rest_framework import serializers
-from .models import GeneralData
-
-class GeneralDataSerializer(serializers.Serializer):
-    class Meta:
-        model = GeneralData()
-        fields = '__all__'
--- ui/js/user/queryBackend.js
+++ ui/js/user/queryBackend.js
@@ -152,5 +152,5 @@ const ApiService = (function() {
 })();
 
 // Initialize and setup cross-tab sync
-// const API = ApiService.getInstance('http://127.0.0.1:8000');
-const API = ApiService.getInstance('https://smartapplicant-backend.onrender.com');
+const API = ApiService.getInstance('http://146.19.133.88:8000');
+// const API = ApiService.getInstance('https://smartapplicant-backend-c4f2.onrender.com');
